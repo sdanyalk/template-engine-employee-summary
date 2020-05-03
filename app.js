85de76a4dab30dfad25f8e2a4bc41f3983826cfd
SDK <s.danyal.k@gmail.com>
2020-05-03 12:37:11 -0700

    add team data to output file

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const path = require("path");
-const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
 const managerHelper = require("./lib/managerHelper");
 const teamHelper = require("./lib/teamHelper");
 
 const startApp = async () => {
     const managerInfo = await managerHelper.askForManagerInfo();
+
     teamHelper.addToTeam(
         new Manager(
             managerInfo.managerName,
