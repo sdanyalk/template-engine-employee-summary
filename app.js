7d834e4ad60342663b103ab02280f200fc1c7d95
SDK <s.danyal.k@gmail.com>
2020-05-08 10:52:23 -0700

    add code comments

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+/** 
+ * This module is the starting point of the program.
+ * */
+
 const Manager = require("./lib/Manager");
 
 const managerHelper = require("./lib/helper/managerHelper");
 const teamHelper = require("./lib/helper/teamHelper");
 
 const startApp = async () => {
+    // Get manager info first.
     const managerInfo = await managerHelper.askForManagerInfo();
 
+    // Add manager info to the global array.
     teamHelper.addToTeam(
         new Manager(
             managerInfo.managerName,
@@ -13,7 +19,9 @@ const startApp = async () => {
             managerInfo.managerEmail,
             managerInfo.managerOfficeNumber));
 
+    // Create rest of the team.
     teamHelper.createTeam();
 }
 
+// Start the app.
 startApp();
